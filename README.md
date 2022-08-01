87e30592884bcf36e115c0f38f7647e26a16315b
rafiqnasrat <rafiqullahnasrat3@gmail.com>
2022-08-01 10:53:06 -0500

    i did my java whit funshan let var

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ also button onclike
 now i start work in css
 i give same style to body
 and also i give same more style card-body class
+i make a function name getPassword
+let characters 
+let password
+
---
+++ js/app.js
@@ -0,0 +1,16 @@
+function getPassword()
+{
+    let chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+?><:.,}{[]1234567890";
+    let passLength = 12;
+    let password = "";
+
+    for(let i=0; i<passLength; i++)
+    {
+        let randNum = Math.floor(Math.random() * chars.length);
+        console.log(randNum);
+
+        password += chars.substring(randNum, randNum + 1);
+        console.log(password);
+    }
+    document.querySelector('#password').value = password;
+}
