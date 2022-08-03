dfa7d200e1bb303c0e066f9ed0e96aad4e5a4a79
rafiqnasrat <rafiqullahnasrat3@gmail.com>
2022-08-03 11:57:10 -0500

    if ind else if for the mix mini

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 
 function getPassword()
 {
+     // Variables
+
     let chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+?><:.,}{[]1234567890";
-    let passLength = 12;
+    let passLength = 0;
     let password = "";
+
+    let PassLen = prompt("Please provide password lenght: 8 - 128");
+
+    if(PassLen === "" || PassLen === null){
+        alert("Password length is required!");
+        return getPassword();
+    }else if(PassLen < 8){
+        alert ("minimum password length is 8 characters");
+        return getPassword();
+    }else if (PassLen >128)
+    {
+        alert("Maximum password length is 128");
+        return getPassword();
+    }
 
     for(let i=0; i<passLength; i++)
     {
