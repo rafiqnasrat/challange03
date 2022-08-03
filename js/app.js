1d7dc9d29c3a06eb3806da2ae4ec31e6bbd14b09
rafiqnasrat <rafiqullahnasrat3@gmail.com>
2022-08-03 12:15:26 -0500

    num spisha char more work

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ function getPassword()
 {
      // Variables
 
-    let chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+?><:.,}{[]1234567890";
+    let chars = "qwertyuiopasdfghjklzxcvbnm";
     let passLength = 0;
     let password = "";
-
+// PROMPT BOX FOR GETTING PASSWORD LENGTH
     let PassLen = prompt("Please provide password lenght: 8 - 128");
-
+ // CHECK FOR  PROMPT VALUES
     if(PassLen === "" || PassLen === null){
         alert("Password length is required!");
         return getPassword();
@@ -19,7 +19,37 @@ function getPassword()
     {
         alert("Maximum password length is 128");
         return getPassword();
+    }else if(PassLen.match(/[azAZ]/g)){
+        alert("Only numeric data is allow");
+        return getPassword();
+    }else
+    {
+        passLength = PassLen;
     }
+
+    // CHECKING FOR LETTER CASE
+    let letterCase = window.confirm("Do you want to have uppercase?");    
+
+    // CHECKING FOR SPECIAL CHARACTERS
+    let specialChar = window.confirm("Do you want special characters in your password?");
+
+    // CHECKING FOR NUMERIC CHARACTERS
+    let numericChar = window.confirm("Do you want numeric characters in your password?");
+
+    if (letterCase)
+    {
+        chars =+ "QWERTYUIOPASDFGHJKLZXCVBNM";
+    }
+
+    if (specialChar)
+    {
+        chars =+ "!@#$%^&*()_+?><:.,}{[]";
+    }
+    
+    if (numericChar)
+{
+    alert =+ "1234567890";
+}
 
     for(let i=0; i<passLength; i++)
     {
